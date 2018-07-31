76db7772238b985f3481b9f115fbb4f6ceceb3d6
Daniel Pérez <djdany.djesus@gmail.com>
2018-07-31 14:50:19 +0200

    Improvements
    Improved functionality and layout finished

--- NoMoreIntros.py
+++ NoMoreIntros.py
@@ -1,14 +1,10 @@
-from imageio.plugins.ffmpeg import download
-download()
-from os import stat
-from os.path import basename
-from stat import ST_SIZE
-from datetime import timedelta
-import wx
-from ObjectListView import ObjectListView, ColumnDefn
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from CLIScript import check_list, cut_video, kill_ffmpeg, delete_files
+import datetime
+import os
+import stat
 import i18n
+import wx
+from ObjectListView import ColumnDefn, ObjectListView
+from imageio.plugins import ffmpeg
 
 
 ########################################################################
@@ -38,7 +34,7 @@ class FileInfo(object):
     # ----------------------------------------------------------------------
     def __init__(self, path, minutes_lenght, size):
         """Constructor"""
-        self.name = path.basename(path)
+        self.name = os.path.basename(path)
         self.path = path
         self.minutes_lenght = minutes_lenght
         self.size = size
@@ -65,34 +61,45 @@ class MainPanel(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.olv.Bind(wx.EVT_KEY_UP, self.OnDelete)
         sizer.Add(self.olv, 1, wx.EXPAND)
+
+        grid1 = wx.GridSizer(1, 3, 1, 10)
+        grid2 = wx.GridSizer(1, 3, 1, 10)
         self.lblCutInitSec = wx.StaticText(
             self, -1, i18n.t('i18n.lblCutInitSec'))
+        self.txtCutInitSec = wx.TextCtrl(self, -1, "0", size=(175, -1))
         self.lblCutEndSec = wx.StaticText(
             self, -1, i18n.t('i18n.lblCutEndSec'))
-        self.txtCutInitSec = wx.TextCtrl(self, -1, "0", size=(175, -1))
         self.txtCutEndSec = wx.TextCtrl(self, -1, "0", size=(175, -1))
         self.chkDelete = wx.CheckBox(self, -1, i18n.t('i18n.chkDelete'))
+        grid1.AddMany([self.lblCutInitSec, self.lblCutEndSec])
+        grid1.AddSpacer(0)
+        grid2.AddMany([self.txtCutInitSec, self.txtCutEndSec, self.chkDelete])
+
         self.btnCut = wx.Button(
             self, -1, i18n.t('i18n.btnCut'), size=(100, 50))
         self.btnCut.Bind(wx.EVT_BUTTON, self.OnCut)
-        sizer.AddMany([self.lblCutInitSec, self.txtCutInitSec,
-                       self.lblCutEndSec, self.txtCutEndSec, self.chkDelete])
+        sizer.AddSpacer(20)
+        sizer.Add(grid1, 0, wx.EXPAND | wx.LEFT | wx.BOTTOM, 10)
+        sizer.Add(grid2, 0, wx.EXPAND | wx.LEFT, 10)
+        sizer.AddSpacer(20)
         sizer.Add(self.btnCut, 0, wx.EXPAND)
         self.SetSizer(sizer)
 
     # ----------------------------------------------------------------------
     def updateDisplay(self, file_list):
+        import CutScript
         """"""
         for path in file_list:
-            file_stats = stat(path)
+            file_stats = os.stat(path)
             try:
-                clip = VideoFileClip(path)
-            except:
+                clip_duration = CutScript.get_video_duration(path)
+            except Exception as e:
+                print(e)
                 wx.MessageBox(i18n.t('i18n.onlyVideosError'), "Error")
                 return
-            minutes_lenght = str(timedelta(
-                seconds=int(clip.duration)))
-            file_size = file_stats[ST_SIZE]
+            minutes_lenght = str(datetime.timedelta(
+                seconds=clip_duration))
+            file_size = file_stats[stat.ST_SIZE]
             if file_size > 1024:
                 file_size = file_size / 1024.0
                 file_size = "%.2f KB" % file_size
@@ -115,6 +122,7 @@ class MainPanel(wx.Panel):
         self.olv.SetObjects(self.file_list)
 
     def OnCut(self, event):
+        import CutScript
         try:
             begin = int(self.txtCutInitSec.GetLineText(1))
             end = int(self.txtCutEndSec.GetLineText(1))
@@ -125,8 +133,8 @@ class MainPanel(wx.Panel):
                 i18n.t('i18n.onlyNumberError'), "Error")
             return
 
-        if(check_list(self.file_list) == True):
-            cutted = cut_video(begin, end, self.file_list)
+        if self.file_list:
+            cutted = CutScript.cut_video(begin, end, self.file_list)
             if(cutted.get('state')):
                 self.file_list.clear()
                 self.updateDisplay(self.file_list)
@@ -136,13 +144,12 @@ class MainPanel(wx.Panel):
             wx.MessageBox(i18n.t('i18n.noCutVideosError'), "Error")
             return
 
-        if(delete == True and cutted.get('state') == True):
-            deleted = delete_files(delete_file_list)
-
-        if(deleted):
-            wx.MessageBox(i18n.t('i18n.deletedOld'), "OK")
-        else:
-            wx.MessageBox(i18n.t('i18n.permisionError'), "Error")
+        if(delete is True and cutted.get('state') is True):
+            try:
+                CutScript.delete_files(delete_file_list)
+                wx.MessageBox(i18n.t('i18n.deletedOld'), "OK")
+            except:
+                wx.MessageBox(i18n.t('i18n.permisionError'), "Error")
 
     def OnDelete(self, event):
         if(event.GetUnicodeKey() == 127 and self.olv.GetSelectedItemCount() > 0):
@@ -162,9 +169,21 @@ class MainFrame(wx.Frame):
         """Constructor"""
         wx.Frame.__init__(self, None, title="NoMoreIntros", size=(570, 400))
         panel = MainPanel(self)
+        ico = wx.Icon('icon.ico', wx.BITMAP_TYPE_ICO)
+        self.SetIcon(ico)
+        self.SetMinSize(wx.Size(570, 400))
         self.Show()
 
 # ----------------------------------------------------------------------
+
+
+def check_ffmpeg():
+    try:
+        import CutScript
+    except:
+        wx.MessageBox(i18n.t('i18n.ffmpegError'), "Error")
+        ffmpeg.download()
+        wx.MessageBox(i18n.t('i18n.ffmpegDownloaded'), "OK")
 
 
 def main():
@@ -174,6 +193,7 @@ def main():
     i18n.load_path.append('./lang/')
     app = wx.App(False)
     frame = MainFrame()
+    check_ffmpeg()
     app.MainLoop()
 
 
